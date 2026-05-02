eb18f8e7021bc2f66955e25e942bd294d15cac2b
hissapinto <hissapinto@hotmail.com>
2026-05-02 17:53:05 -0300

    Solution for Roman to Integer

---
+++ RomanToInteger/RomanToInteger.java
@@ -0,0 +1,39 @@
+package RomanToInteger;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println("\n\n" + s.romanToInt("MCMXCIV"));
+    }
+}
+
+class Solution {
+    public int romanToInt(String s) {
+        int r = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i>= 0; i--) {
+            int now = charValue(s.charAt(i));
+
+            r += (now >= prev) ? now : -now;
+            prev = now;
+        }
+
+        return r;
+    }
+
+    private int charValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+}
