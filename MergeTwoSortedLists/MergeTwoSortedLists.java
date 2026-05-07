6825a2a0a14e73cb124d93e6a678b408c39d19ec
hissapinto <hissapinto@hotmail.com>
2026-05-06 22:36:52 -0300

    solution for Merge Two Sorted List

---
+++ MergeTwoSortedLists/MergeTwoSortedLists.java
@@ -0,0 +1,42 @@
+package MergeTwoSortedLists;
+
+
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        ListNode result = s.mergeTwoLists(list1, list2);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+ 
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null || list2 == null) {
+            return (list1 != null) ? list1 : list2;
+        }
+
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list2.next, list1);
+            return list2;
+        }
+    }
+}
