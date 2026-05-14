f173d5a7a194cdf3564a8f5f58acde9847d0aa3a
hissapinto <hissapinto@hotmail.com>
2026-05-14 12:56:21 -0300

    solution for Remove Element

---
+++ RemoveElement/RemoveElement.java
@@ -0,0 +1,31 @@
+package RemoveElement;
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int j = 0;
+
+        for(int i = 0; i<nums.length; i++) {
+            if(nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+}
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+
+        Solution s = new Solution();
+        int k = s.removeElement(nums, val);
+
+        System.out.println("k = " + k);
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
