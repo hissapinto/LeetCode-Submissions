6407bf36506d9088c7f6329189ebe0be381c2ae4
hissapinto <isabelahissap@gmail.com>
2026-06-24 11:58:25 -0300

    LongestConsecutiveSequence feito em O(n)

---
+++ LongestConsecutiveSequence/LongestConsecutiveSequence.java
@@ -0,0 +1,56 @@
+package LongestConsecutiveSequence;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.longestConsecutive(new int[]{100,4,200,1,3,2}));    // esperado: 4
+        System.out.println(s.longestConsecutive(new int[]{0,3,7,2,5,8,4,6,0,1})); // esperado: 9
+        System.out.println(s.longestConsecutive(new int[]{}));                     // esperado: 0
+        System.out.println(s.longestConsecutive(new int[]{1}));                    // esperado: 1
+    }
+}
+
+class Solution {
+
+    //Tempo O(n)
+    public int longestConsecutive(int[] nums) {
+        //resp = 0 que recebe a maior sequencia
+        //aux para contabilizar sequencia atual
+        //booleano que indica se estou em uma sequencia ou nao
+        int resp = 0;
+        int aux = 0;
+        int len = nums.length;
+        boolean emSeq = false;
+
+        //Mapear todos os numeros, sendo key - o numero
+        Set <Integer> set = new HashSet<>();
+        for (int j = 0; j < len; j++) {
+            set.add(nums[j]);
+        }
+
+        //Percorrer no set pois nao tem duplicatas
+        for(int key : set) {
+            int prox = key + 1;
+            if(!set.contains(key - 1)) { //Se nao tem predecessor = nova seq
+                emSeq = true;
+                aux = 1;
+            }
+            while(emSeq) { //É no máximo O(2n) = O(n)
+                if(set.contains(prox)) {
+                    aux++;
+                    prox++;
+                } else {
+                    emSeq = false;
+                }
+            }
+            if (aux > resp) {resp = aux;}
+        }
+ 
+        return resp;
+    }
+}
+
