d788ae09a52f0d616b51a3373a34d3682fe2311d
hissapinto <isabelahissap@gmail.com>
2026-07-11 13:01:44 -0300

    ContainerWithMostWater optimal solution

--- ContainerWithMostWater/ContainerWithMostWater.java
+++ ContainerWithMostWater/ContainerWithMostWater.java
@@ -8,6 +8,8 @@ public class ContainerWithMostWater {
         System.out.println(s.maxArea(new int[]{1,1}));                 // esperado: 1
         System.out.println(s.maxArea(new int[]{1,2,4,3}));             // esperado: 4
         System.out.println(s.maxArea(new int[]{4,3,2,1,4}));           // esperado: 16
+        System.out.println(s.maxAreaOpt(new int[]{2,7,9,3,2,0,6,8,4,2}));
+        System.out.println(s.maxAreaOpt(new int[]{1,4,6,8,9,6,5,0,8,1,4,3,2,6,5}));
     }
 }
 
@@ -31,6 +33,22 @@ class Solution {
                 r--;
             }
 
+        }
+
+        return resp;
+    }
+
+    public int maxAreaOpt(int[] height) {
+        int resp = 0;
+        int l = 0;
+        int r = height.length - 1;
+
+        while (l < r) {
+            int area =  Math.min(height[l], height[r]) * (r - l);
+            if (area > resp) resp = area;
+
+            if (height[l] > height[r]) r--;
+            else l++;
         }
 
         return resp;
